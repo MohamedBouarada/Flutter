3458f78dcecb367fa4b50c5610aae55dcf7835ed
MohamedBouarada <bouarada7.mb@gmail.com>
2022-03-19 20:01:52 +0100

    Connecting functions And buttons + Anonymous functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('answer chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
@@ -18,9 +24,21 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             Text('The question!'),
-            RaisedButton(child: Text('Answer 1'), onPressed: null),
-            RaisedButton(child: Text('Answer 2'), onPressed: null),
-            RaisedButton(child: Text('Answer 3'), onPressed: null),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //...
+                print('Answer 3 chosen!');
+              },
+            ),
           ],
         ),
       ),
